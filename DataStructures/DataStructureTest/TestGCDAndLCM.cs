02e57e32313c13be7533b93dc47476ce972375cf
LocInside <mail2dhanilan@gmail.com>
2016-07-11 14:19:03 +0530

    Initial Commit.Contains GCD .LCM . Linked List ,Sorted Linked List

---
+++ DataStructures/DataStructureTest/TestGCDAndLCM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures;
+
+namespace DataStructureTest
+{
+    [TestClass]
+    public class TestGCDAndLCM
+    {
+        [TestMethod]
+        public void TestEuclid()
+        {
+           Assert.AreEqual( EuclidsAlgorithm.GetGCD(78,66),6);
+        }
+
+        [TestMethod]
+        public void TestLCMWithGCD()
+        {
+            Assert.AreEqual(EuclidsAlgorithm.LCMUsingGCD(12, 15), 60);            
+        }
+    }
+}
